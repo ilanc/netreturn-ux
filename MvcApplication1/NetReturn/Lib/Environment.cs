590d6e348e311f2e5080b5789b094f0803f9e654
Ilan <ilan@netreturn.co.za>
2013-06-24 16:54:29 +0200

    Basic shell = menu with 3 apps

---
+++ MvcApplication1/NetReturn/Lib/Environment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Configuration;
+
+namespace NetReturn.Lib
+{
+    public static class Environment
+    {
+        static string[] ProductionServers = new string[] { "ilancopelyn" };
+        static bool _IsLive = false;
+
+        static Environment()
+        {
+            string isLive = GetConfigSetting("IsLive");
+            if (!String.IsNullOrEmpty(isLive))
+            {
+                _IsLive = Boolean.Parse(isLive);
+            }
+        }
+
+        private static string GetConfigSetting(string name)
+        {
+            string path = WebConfigurationManager.AppSettings[name];
+            /*
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new WebException("Missing " + name + " setting in web.config.");
+            }
+             */
+            return path;
+        }
+
+        private static bool HasConfigSetting(string name)
+        {
+            string path = WebConfigurationManager.AppSettings[name];
+            return (!string.IsNullOrEmpty(path));
+        }
+
+        public static bool IsLive()
+        {
+            //string host = Dns.GetHostName().ToLower();
+            //return ProductionServers.Contains(host);
+            return _IsLive;
+        }
+    }
+}
---
+++ MvcApplication1/NetReturn/Lib/UrlHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+using System.Collections.Specialized;
+using System.IO;
+using System.Web.Mvc;
+
+namespace NetReturn.Lib
+{
+    public static class UrlHelper
+    {
+        public static string GetURLRoot()
+        {
+            string host = (HttpContext.Current.Request.Url.IsDefaultPort) ?
+                HttpContext.Current.Request.Url.Host :
+                HttpContext.Current.Request.Url.Authority;
+            host = String.Format("{0}://{1}", HttpContext.Current.Request.Url.Scheme, host);
+            return host;
+        }
+
+        public static string GetURLAppPath()
+        {
+            string host = GetURLRoot();
+            if (HttpContext.Current.Request.ApplicationPath == "/")
+                return host;
+            else
+                return host + HttpContext.Current.Request.ApplicationPath;
+        }
+
+        // Example: Url.Action("action", "controller", Request.QueryString.ToRouteValues(new { id = 0 }));
+        public static RouteValueDictionary ToRouteValues(this NameValueCollection col, Object obj)
+        {
+            var values = obj == null ? new RouteValueDictionary() : new RouteValueDictionary(obj);
+            if (col != null)
+            {
+                foreach (string key in col)
+                {
+                    // values passed in object override those already in collection
+                    if (!values.ContainsKey(key))
+                    {
+                        values[key] = col[key];
+                    }
+                }
+            }
+            return values;
+        }
+
+        public static RouteValueDictionary ToRouteValues(this NameValueCollection col)
+        {
+            return col.ToRouteValues((Object)null);
+        }
+
+        public static RouteValueDictionary ToRouteValuesWithCacheBuster(this NameValueCollection col)
+        {
+            return col.ToRouteValues(new { _dc = Guid.NewGuid().ToString() });
+        }
+
+        public static RouteValueDictionary ToRouteValuesWithCacheBuster(this NameValueCollection col, Object obj)
+        {
+            // NOTE: Your Object must have Properties, NOT fields e.g. with {get;set;} afterwards - see C:\netreturn.co.za\NetReturn\NetReturn\Models\Output\PriceInputsVM.cs
+            var ret = col.ToRouteValues(obj);
+            ret["_dc"] = Guid.NewGuid().ToString();
+            return ret;
+        }
+
+        public static string JSScriptWithFileModTime(string src, bool srcHasQueryString)
+        {
+            string file = HttpContext.Current.Server.MapPath(src);
+            FileInfo i = new FileInfo(file);
+            if (srcHasQueryString)
+            {
+                return src + "&_dc=" + i.LastWriteTime.Ticks;
+            }
+            else
+            {
+                return src + "?_dc=" + i.LastWriteTime.Ticks;
+            }
+        }
+
+        public static string JSScriptWithFileModTime(this HtmlHelper h, string src, bool srcHasQueryString)
+        {
+            return JSScriptWithFileModTime(src, srcHasQueryString);
+        }            
+    }
+}
